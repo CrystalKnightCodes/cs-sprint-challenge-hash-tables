a27dd32ff5cf4460f943cf5e5c5c7afb6e31d03f
Crystal Knight <crystalknightcodes@gmail.com>
2020-09-04 12:03:11 -0400

    Completed function to add package weights together to equal the weight limit.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,16 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
+
+    for weight in range(length):
+        current_weight = weights[weight]
+        remaining_weight = limit - current_weight
+
+        if remaining_weight in cache:
+            if cache[remaining_weight] > weight:
+                return[cache[remaining_weight], weight]
+            else:
+                return [weight, cache[remaining_weight]]
+        else:
+            cache[current_weight] = weight
 
     return None
